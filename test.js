9e3c6b87b5baa9770357ebf9ac3a36b2d07aeb44
ccorcoveanu <cristi.corcoveanu@gmail.com>
2020-02-06 14:32:29 +0200

    update test file and keep original data in transaction

--- test.js
+++ test.js
@@ -2,9 +2,7 @@
 
 const account = require('./src/account');
 const transaction = require('./src/transaction');
-const { derivePath, getMasterKeyFromSeed, getPublicKey } = require('ed25519-hd-key');
 
-const hexSeed = 'fffcf9f6f3f0edeae7e4e1dedbd8d5d2cfccc9c6c3c0bdbab7b4b1aeaba8a5a29f9c999693908d8a8784817e7b7875726f6c696663605d5a5754514e4b484542';
 const receiver = '53669be65aac358a6add8e8a8b1251bb994dc1e4a0cc885956f5ecd53396f0d8';
 
 // Build a fresh account
@@ -14,9 +12,6 @@ const sk = 'ZjdhMDNhNDczNjYyZjA2NzczZDA0OTIyZWNkODc4MWZlYTE2ZmVmNTQzNTMxODI4MTQ0
 const senderAcc = new account();
 const hexSk = Buffer.from(sk, 'base64').toString();
 const hexPrivate = Buffer.from(hexSk, 'hex');
-
-// All good here :)
-
 senderAcc.loadFromSeed(hexPrivate);
 
 // // Transaction with gasPrice, gasLimit, Data
@@ -29,8 +24,6 @@ const senderAcc2 = new account();
 const mnemonic = senderAcc2.generateMnemonic();
 console.log(mnemonic);
 senderAcc2.loadFromMnemonic(mnemonic);
-
-senderAcc2.generateKeyFile('password');
 
 const myNewTx2 = new transaction(0, senderAcc2.publicKeyAsString(), receiver, "999", 10, 100000, "!!!!!");
 const txBeforeSigning2 = myNewTx2.prepareForSigning();
